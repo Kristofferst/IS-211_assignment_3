a6526b76150959cd4498f7396d7e2881c3c5c083
Kristofferst <61408259+Kristofferst@users.noreply.github.com>
2022-04-26 20:40:28 +0200

    Done? Might deserve som smarter printing

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,50 @@
+import Generic.Graph;
+import Generic.Vertex;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        networkManagementProblemA();
+
+    }
+
+    private static void networkManagementProblemA(){
+        /*
+        A. Network management
+         */
+
+        Graph<String> network = new Graph<>();
+        // From A
+        network.addEdge("A", "B", true, 10);
+        network.addEdge("A", "C", true, 5);
+        network.addEdge("A", "D", true, 9999);
+        network.addEdge("A", "E", true, 3);
+        network.addEdge("A", "F", true, 12);
+        // From B
+        network.addEdge("B", "C", true, 17);
+        network.addEdge("B", "D", true, 9);
+        network.addEdge("B", "E", true, 17);
+        network.addEdge("B", "F", true, 12);
+        // From C
+        network.addEdge("C", "D", true, 35);
+        network.addEdge("C", "E", true, 3);
+        network.addEdge("C", "F", true, 12);
+        // From D
+        network.addEdge("D", "E", true, 9999);
+        network.addEdge("D", "F", true, 12);
+        // From E
+        network.addEdge("E", "F", true, 12);
+        //Dijkstra<String> networkDijkstra = new Dijkstra<>(network, network.getVertex("A"));
+        //networkDijkstra.ShortestPath(network.getVertex("D"));
+        //networkDijkstra.ResultPrinter();
+
+        for (Vertex<String> vertex: network.getVertices()){
+            Dijkstra<String> networkDijkstra = new Dijkstra<>(network, vertex);
+            for (Vertex<String> destinationVertex: network.getVertices()){
+                networkDijkstra.ShortestPath(destinationVertex);
+                networkDijkstra.ResultPrinter();
+            }
+        }
+
     }
 }
